9573894c7247a30b72aeaa7d873a9ac77c765b54
Thierry Blais <tb@thierryblais.com>
2016-03-20 23:53:47 -0400

    use gulp inject

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,14 @@ var connect = require('gulp-connect');
 var open = require('gulp-open');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var inject = require('gulp-inject');
+var sources = {
+	js: ['js/*.js', 'js/**/*.js'],
+	css: ['css/*.css'],
+	sass: ['sass/*.scss'],
+	html: ['index.html', 'partials/*.html']
+}
+
  
 gulp.task('connect', function() {
   connect.server({
@@ -25,22 +33,33 @@ gulp.task('open', ['connect'], function () {
             uri: 'http://localhost:4567/index.html',
             app: 'chrome' 
         }));
+});
+
+gulp.task('inject', function () {
+	var target = gulp.src('index.html');
+	var allSources = sources.js.concat(sources.css);
+
+	console.log(allSources);
+
+	return target
+			.pipe(inject(gulp.src(allSources)))
+			.pipe(gulp.dest(''));
 
 });
 
 gulp.task('sass', function () {
-  return gulp.src('sass/*.scss')
+  return gulp.src(sources.sass)
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(concat('tb.css'))
     .pipe(gulp.dest('css'));
 });
  
 gulp.task('watch', function () {
-  gulp.watch(['index.html', 'partials/*.html'], ['html']);
-  gulp.watch('sass/*.scss', ['sass', 'html']);
+  gulp.watch(sources.html, ['html']);
+  gulp.watch(sources.sass, ['sass', 'html']);
 });
  
-gulp.task('default', ['sass', 'open', 'watch']);
+gulp.task('default', ['sass', 'inject', 'open', 'watch']);
 
  
 
