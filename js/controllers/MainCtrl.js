13a2f559a1b315463658cbcc3a9931b7e2d6caf6
Thierry Blais <tb@thierryblais.com>
2016-03-21 21:07:14 -0400

    added main page

--- js/controllers/MainCtrl.js
+++ js/controllers/MainCtrl.js
@@ -4,9 +4,11 @@
 	function MainCtrl() {
 		var ctrl = this;
 
-		ctrl.hello = 'Hi';
-
-
+		ctrl.footerLinks = [
+			{name: 'LinkedIn', icon: 'ion-social-linkedin', url: 'https://ca.linkedin.com/in/thierry-blais-93142323'},
+			{name: 'Twitter', icon: 'ion-social-twitter', url: 'https://twitter.com/thierryblais'},
+			{name: 'GitHub', icon: 'ion-social-github', url: 'https://github.com/Likwidt'}
+		];
 	}
 
 	angular
